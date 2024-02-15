e8e1b47dc50bd3f389692e3ad7544e561032c424
anyacallissta <vtjhs18@gmail.com>
2024-02-15 18:55:13 +0700

    add fungsi menghitung stok RG4

--- ASDJobsheet1/Fungsi05.java
+++ ASDJobsheet1/Fungsi05.java
@@ -3,6 +3,7 @@ package ASDJobsheet1;
 public class Fungsi05 {
     static int [][] n = new int[4][4];
     static int penjualanRG1, penjualanRG2, penjualanRG3, penjualanRG4;
+    static int [][] s = new int[1][4];
     static int hargaAglonema = 75000;
     static int hargaKeladi = 50000;
     static int hargaAlocasia = 60000;
@@ -38,6 +39,10 @@ public class Fungsi05 {
         n[3][2] = 12;
         n[3][3] = 9;
         penjualanRG4 = (n[3][0] * hargaAglonema) + (n[3][1] * hargaKeladi) + (n[3][2] * hargaAlocasia) + (n[3][3] * hargaMawar);
+        s[0][0] = n[3][0] - 1;
+        s[0][1] = n[3][1] - 2;
+        s[0][2] = n[3][2] - 0;
+        s[0][3] = n[3][3] - 5;
     }
     public static void main(String[] args) {
         RoyalGarden1();
@@ -46,9 +51,11 @@ public class Fungsi05 {
         RoyalGarden4();
 
         System.out.println(" Nama Toko      Aglonema   Keladi   Alocasia   Mawar   Penjualan");
-        System.out.println("RoyalGarden1       " +n[0][0]+"         "+n[0][1] +"        "+n[0][2]+"        "+n[0][3]+"      "+penjualanRG1);
-        System.out.println("RoyalGarden2        " +n[1][0]+"        "+n[1][1] +"         "+n[1][2]+"       "+n[1][3]+"      "+penjualanRG2);
-        System.out.println("RoyalGarden3        " +n[2][0]+"        "+n[2][1] +"        "+n[2][2]+"        "+n[2][3]+"      "+penjualanRG3);
-        System.out.println("RoyalGarden4        " +n[3][0]+"         "+n[3][1] +"        "+n[3][2]+"        "+n[3][3]+"      "+penjualanRG4);
+        System.out.println("RoyalGarden1       " +n[0][0]+"         "+n[0][1]+"        "+n[0][2]+"        "+n[0][3]+"      "+penjualanRG1);
+        System.out.println("RoyalGarden2        " +n[1][0]+"        "+n[1][1]+"         "+n[1][2]+"       "+n[1][3]+"      "+penjualanRG2);
+        System.out.println("RoyalGarden3        " +n[2][0]+"        "+n[2][1]+"        "+n[2][2]+"        "+n[2][3]+"      "+penjualanRG3);
+        System.out.println("RoyalGarden4        " +n[3][0]+"         "+n[3][1]+"        "+n[3][2]+"        "+n[3][3]+"      "+penjualanRG4);
+        System.out.println("================================================================");
+        System.out.println("Stock RoyalGarden4  " +s[0][0]+"         "+s[0][1]+"        "+s[0][2]+"        "+s[0][3]);
     }
 }
