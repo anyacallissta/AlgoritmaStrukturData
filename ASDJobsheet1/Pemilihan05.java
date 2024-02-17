0ad7d98d305d51aa8d4d8c68d3040d05c0ad9f28
anyacallissta <vtjhs18@gmail.com>
2024-02-17 10:14:05 +0700

    memperbaiki logika nilai tidak valid

--- ASDJobsheet1/Pemilihan05.java
+++ ASDJobsheet1/Pemilihan05.java
@@ -20,38 +20,38 @@ public class Pemilihan05 {
             
         if (tugas < 0 || tugas > 100 || kuis < 0 && kuis > 100 || uts < 0 && uts > 100 || uas < 0 && uas > 100) {
             System.out.print("Nilai tidak valid.");
+        } else {
+            float nilaiAkhir = (tugas * 0.2F) + (kuis * 0.2F) + (uts * 0.3F) + (uas * 0.4F);
+            String nilaiHuruf = sc.nextLine();
+    
+            if (nilaiAkhir > 80 && nilaiAkhir <= 100) {
+                nilaiHuruf = "A";
+            } else if (nilaiAkhir >73 && nilaiAkhir <=80) {
+                nilaiHuruf = "B+";
+            } else if (nilaiAkhir >65 && nilaiAkhir <=73) {
+                nilaiHuruf = "B";
+            } else if (nilaiAkhir >60 && nilaiAkhir <=65) {
+                nilaiHuruf = "C+";
+            } else if (nilaiAkhir >50 && nilaiAkhir <=60) {
+                nilaiHuruf = "C";
+            } else if (nilaiAkhir >39 && nilaiAkhir <=50) {
+                nilaiHuruf = "D";
+            } else {
+                nilaiHuruf = "E";
+            }
+            
+            System.out.println("\n===================================");
+            System.out.println("Nilai Akhir : " + nilaiAkhir);
+            System.out.println("Nilai Huruf : " + nilaiHuruf);
+            System.out.println("===================================");
+    
+            if (nilaiHuruf == "A" || nilaiHuruf == "B+" || nilaiHuruf == "B" || nilaiHuruf == "C+" || nilaiHuruf == "C") {
+                System.out.println("Anda dinyatakan 'LULUS'");
+            } else {
+                System.out.println("Anda dinyatakan 'TIDAK LULUS'");
+            }
+
         }      
 
-        float nilaiAkhir = (tugas * 0.2F) + (kuis * 0.2F) + (uts * 0.3F) + (uas * 0.4F);
-        String nilaiHuruf = sc.nextLine();
-
-        if (nilaiAkhir > 80 && nilaiAkhir <= 100) {
-            nilaiHuruf = "A";
-        } else if (nilaiAkhir >73 && nilaiAkhir <=80) {
-            nilaiHuruf = "B+";
-        } else if (nilaiAkhir >65 && nilaiAkhir <=73) {
-            nilaiHuruf = "B";
-        } else if (nilaiAkhir >60 && nilaiAkhir <=65) {
-            nilaiHuruf = "C+";
-        } else if (nilaiAkhir >50 && nilaiAkhir <= 60) {
-            nilaiHuruf = "C";
-        } else if (nilaiAkhir >39 && nilaiAkhir <=50) {
-            nilaiHuruf = "D";
-        } else {
-            nilaiHuruf = "E";
-        }
-
-        
-        
-        System.out.println("\n===================================");
-        System.out.println("Nilai Akhir : " + nilaiAkhir);
-        System.out.println("Nilai Huruf : " + nilaiHuruf);
-        System.out.println("===================================");
-
-        if (nilaiHuruf == "A" || nilaiHuruf == "B+" || nilaiHuruf == "B" || nilaiHuruf == "C+" || nilaiHuruf == "C") {
-            System.out.println("Anda dinyatakan 'LULUS'");
-        } else {
-            System.out.println("Anda dinyatakan 'TIDAK LULUS'");
-        }
     }
 }
