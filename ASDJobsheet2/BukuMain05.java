59761e358b0c2ab1c574709d07b2320cccce0af1
anyacallissta <vtjhs18@gmail.com>
2024-02-24 16:38:40 +0700

    menambahkan method pada bukuAnyaCallissta

--- ASDJobsheet2/BukuMain05.java
+++ ASDJobsheet2/BukuMain05.java
@@ -13,22 +13,26 @@ public class BukuMain05 {
         bk1.tampilInformasi();
         bk1.terjual(5);
         bk1.gantiHarga(60000);
-        System.out.println();
         bk1.hitungHargaTotal(5);
         bk1.hitungDiskon();
         bk1.hitungHargaBayar();
+        System.out.println();
         bk1.tampilInformasi();
 
         Buku05 bk2 = new Buku05("Self Reward", "Maheera Ayesha", 160, 29, 59000);
         bk2.terjual(11);
-        System.out.println();
-        bk2.hitungHargaTotal(5);
+        bk2.hitungHargaTotal(11);
         bk2.hitungDiskon();
         bk2.hitungHargaBayar();
+        System.out.println();
         bk2.tampilInformasi();
 
-        System.out.println();
         Buku05 bukuAnyaCallissta = new Buku05("Divergent", "Veronica Roth", 487, 17, 75000);
+        bukuAnyaCallissta.terjual(7);
+        bukuAnyaCallissta.hitungHargaTotal(7);
+        bukuAnyaCallissta.hitungDiskon();
+        bukuAnyaCallissta.hitungHargaBayar();
+        System.out.println();
         bukuAnyaCallissta.tampilInformasi();
     }
 }
