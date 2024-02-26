2f81e4b6d0458ba0560c856776e8e1236777e1f4
anyacallissta <vtjhs18@gmail.com>
2024-02-26 09:46:26 +0700

    fix output percobaan 1

--- ASDJobsheet3/ArrayObjects05.java
+++ ASDJobsheet3/ArrayObjects05.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class ArrayObjects05 {
     
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         PersegiPanjang05[] ppArray = new PersegiPanjang05[3];
 
         ppArray[0] = new PersegiPanjang05();
@@ -16,8 +19,8 @@ public class ArrayObjects05 {
         ppArray[2].lebar = 20;
 
         System.out.println("Persegi panjang ke-0, panjang: " + ppArray[0].panjang + ", lebar: " + ppArray[0].lebar);
-        System.out.println("Persegi panjang ke-0, panjang: " + ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
-        System.out.println("Persegi panjang ke-0, panjang: " + ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
+        System.out.println("Persegi panjang ke-1, panjang: " + ppArray[1].panjang + ", lebar: " + ppArray[1].lebar);
+        System.out.println("Persegi panjang ke-2, panjang: " + ppArray[2].panjang + ", lebar: " + ppArray[2].lebar);
     }
     
 }
