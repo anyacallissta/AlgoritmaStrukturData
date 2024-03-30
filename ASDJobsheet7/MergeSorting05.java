219b4e7bdd544250645b95161db079df916ae314
anyacallissta <vtjhs18@gmail.com>
2024-03-30 18:36:26 +0700

    percobaan 3

---
+++ ASDJobsheet7/MergeSorting05.java
@@ -0,0 +1,67 @@
+package ASDJobsheet7;
+
+public class MergeSorting05 {
+
+    public void mergeSort(int[] data) {
+        sort(data, 0, data.length - 1);
+    }
+
+    public void merge(int[] data, int left, int middle, int right) {
+        int[] temp = new int[data.length];
+        for (int i = left; i <= right; i++) {
+            temp[i] = data[i];
+        }
+        int a = left;
+        int b = middle + 1;
+        int c = left;
+
+        while (a <= middle && b <= right) {
+            if (temp[a] <= temp[b]) {
+                data[c] = temp[a];
+                a++;
+            } else {
+                data[c] = temp[b];
+                b++;
+            }
+            c++;
+        }
+
+        int s = middle - a;
+        for (int i = 0; i <= s; i++) {
+            data[c + i] = temp[a + i];
+        }
+    }
+
+    public void sort(int data[], int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            sort(data, left, middle);
+            sort(data, middle + 1, right);
+            merge(data, left, middle, right);
+        }
+    }
+
+    public void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static class MergeSortMain05 {
+        public static void main(String[] args) {
+            int data[] = {10, 40, 30, 50, 70, 20, 100, 90};
+            System.out.println("Sorting dengan merge sort");
+            System.out.println("=========================");
+            MergeSorting05 mSort = new MergeSorting05();
+            System.out.println("Data awal");
+            mSort.printArray(data);
+            System.out.println("=========================");
+            mSort.mergeSort(data);
+            System.out.println("Setelah diurutkan");
+            mSort.printArray(data);
+        }
+        
+    }
+    
+}
