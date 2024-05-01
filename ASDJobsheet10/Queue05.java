f513efa5376b12ae1d524610e11db6a5e31e064a
anyacallissta <vtjhs18@gmail.com>
2024-05-01 20:50:44 +0700

    percobaan 1

---
+++ ASDJobsheet10/Queue05.java
@@ -0,0 +1,152 @@
+package ASDJobsheet10;
+
+import java.util.Scanner;
+
+public class Queue05 {
+    int[] data;
+    int front, rear, size, max;
+    
+    public Queue05(int n) {
+        max = n;
+        data = new int[max];
+        size = 0;
+        front = rear = -1;
+    }
+
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void peek() {
+        if (!isEmpty()) {
+            System.out.println("Elemen terdepan: " + data[front]);
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void print() {
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            int i = front;
+            while (i != rear) {
+                System.out.print(data[i] + " ");
+                i = (i + 1) % max;
+            }
+            System.out.println(data[i] + " ");
+            System.out.println("Jumlah elemen: " + size);
+        }
+    }
+    
+    public void clear() {
+        if (!isEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+
+    public void enqueue(int dt) {
+        if (isFull()) {
+            System.out.println("Queue sudah penuh");
+        } else {
+            if (isEmpty()) {
+                front = rear = 0;
+            } else {
+                if (rear == max - 1) {
+                    rear = 0;
+                } else {
+                    rear++;
+                }
+            }
+            data[rear] = dt;
+            size++;
+        }
+    }
+
+    public int dequeue() {
+        int dt = 0;
+        if (isEmpty()) {
+            System.out.println("Queue masih kosong");
+        } else {
+            dt = data[front];
+            size--;
+            if (isEmpty()) {
+                front = rear = -1;
+            } else {
+                if (front == max - 1) {
+                    front = 0;
+                } else {
+                    front++;
+                }
+            }
+        }
+        return dt;
+    }
+
+    public static class queueMain {
+
+        public static void menu() {
+            System.out.println("Masukkan operasi yang diinginkan: ");
+            System.out.println("1. Enqueue");
+            System.out.println("2. Dequeue");
+            System.out.println("3. Print");
+            System.out.println("4. Peek");
+            System.out.println("5. Clear");
+            System.out.println("-------------------------------------");
+        }
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Masukkan kapasitas queue: ");
+            int n = sc.nextInt();
+            Queue05 Q = new Queue05(n);
+            int pilih;
+
+            do {
+                menu();
+                pilih = sc.nextInt();
+                switch (pilih) {
+                    case 1:
+                        System.out.print("Masukkan data baru: ");
+                        int dataMasuk = sc.nextInt();
+                        Q.enqueue(dataMasuk);
+                        break;
+
+                    case 2:
+                        int dataKeluar = Q.dequeue();
+                        if (dataKeluar != 0) {
+                            System.out.println("Data yang dikeluarkan: " + dataKeluar);
+                            break;
+                        }
+
+                    case 3:
+                        Q.print();
+                        break;
+
+                    case 4:
+                        Q.peek();
+                        break;
+
+                    case 5:
+                        Q.clear();
+                        break;
+                }
+            } while (pilih == 1 || pilih == 2 || pilih == 3 || pilih == 4 || pilih == 5);
+        }
+    }
+}
