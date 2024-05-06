e6ee9f70390b22aa18331bd3389c7d615d532642
anyacallissta <vtjhs18@gmail.com>
2024-05-06 10:02:29 +0700

    percobaan 1

---
+++ ASDJobsheet11/SLLMain.java
@@ -0,0 +1,18 @@
+package ASDJobsheet11;
+
+public class SLLMain {
+    public static void main(String[] args) {
+        SingleLinkedList singLL = new SingleLinkedList();
+        singLL.print();
+        singLL.addFirst(890);
+        singLL.print();
+        singLL.addLast(760);
+        singLL.print();
+        singLL.addFirst(700);
+        singLL.print();
+        singLL.insertAfter(700, 999);
+        singLL.print();
+        singLL.inserAt(3, 833);
+        singLL.print();
+    }
+}
