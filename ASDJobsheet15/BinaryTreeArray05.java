761033262001a6829e21f69e9336268e5af83244
anyacallissta <vtjhs18@gmail.com>
2024-05-30 20:28:08 +0700

    tugas no 5 jobsheet P15 (add method add(int data), traversePreOrder(), dan traversePostOrder())

--- ASDJobsheet15/BinaryTreeArray05.java
+++ ASDJobsheet15/BinaryTreeArray05.java
@@ -6,6 +6,7 @@ public class BinaryTreeArray05 {
 
     public BinaryTreeArray05() {
         data = new int[10];
+        idxLast = -1;
     }
 
     void populateData(int data[], int idxLast) {
@@ -13,11 +14,36 @@ public class BinaryTreeArray05 {
         this.idxLast = idxLast;
     }
 
-    void traverseInOrder(int idxStart) {
+    void add(int dataTree) {
+        if (idxLast < data.length - 1) {
+            idxLast++;
+            data[idxLast] = dataTree;
+        } else {
+            System.out.println("Tree is full!");
+        }
+    }
+
+    void traverseInOrder(int idxStart) { //traversal left - print - traversal right
         if (idxStart <= idxLast) {
-            traverseInOrder(2 * idxStart + 1);
-            System.out.print(data[idxStart] + " ");
-            traverseInOrder(2 * idxStart + 2);
+            traverseInOrder(2 * idxStart + 1); //traversal left subtree
+            System.out.print(data[idxStart] + " "); //print data in root
+            traverseInOrder(2 * idxStart + 2); //traversal right subtree
+        }
+    }
+
+    void traversePreOrder(int idxStart) { //print - traversal left - traversal right
+        if (idxStart <= idxLast) {
+            System.out.print(data[idxStart] + " "); //print data in root
+            traverseInOrder(2 * idxStart + 1); //traversal left subtree
+            traverseInOrder(2 * idxStart + 2); //traversal right subtree
+        }
+    }
+
+    void traversePostOrder(int idxStart) { //traversal left - traversal right - print
+        if (idxStart <= idxLast) {
+            traverseInOrder(2 * idxStart + 1); //traversal left subtree
+            traverseInOrder(2 * idxStart + 2); //traversal right subtree
+            System.out.print(data[idxStart] + " "); //print data in root
         }
     }
 }
--- ASDJobsheet15/BinaryTreeArrayMain05.java
+++ ASDJobsheet15/BinaryTreeArrayMain05.java
@@ -3,11 +3,21 @@ package ASDJobsheet15;
 public class BinaryTreeArrayMain05 {
     public static void main(String[] args) {
         BinaryTreeArray05 bta = new BinaryTreeArray05();
-        int[] data = {6, 4, 8, 3, 5, 7, 9, 0, 0, 0};;
-        int idxLast = 6;
-        bta.populateData(data, idxLast);
-        System.out.print("\nIn Order Traversal : ");
+        // int[] data = {6, 4, 8, 3, 5, 7, 9, 0, 0, 0};
+        // int idxLast = 6;
+        // bta.populateData(data, idxLast);
+        bta.add(6);
+        bta.add(4);
+        bta.add(8);
+        bta.add(3);
+        bta.add(5);
+        bta.add(7);
+        bta.add(9);
+        System.out.print("\nInOrder Traversal   : ");
         bta.traverseInOrder(0);
-        System.out.println("\n");
+        System.out.print("\nPreOrder Traversal  : ");
+        bta.traversePreOrder(0);
+        System.out.print("\nPostOrder Traversal : ");
+        bta.traversePostOrder(0);
     }
 }
